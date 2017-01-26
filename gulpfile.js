3d2a5e9b2cd01bb7359af781a89c56acd1346a4c
Clara <claraho@live.ca>
2017-01-25 21:51:39 -0800

    Started on jQuery

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'), s
+	rename = require('gulp-rename'), 
 	browserSync = require('browser-sync'),
 	eslint = require('gulp-eslint'),
 	plumber = require('gulp-plumber'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssnano = require('gulp-cssnano');
 
 var plumberErrorHandler = {
   errorHandler: notify.onError({
@@ -27,7 +30,7 @@ gulp.task('browser-sync', function() {
 			baseDir: './'
 		}
 	});
-	gulp.watch(['css/*css', 'js/*js']).on('change', browserSync.reload);
+	gulp.watch(['build/css/*.css', 'js/*.js']).on('change', browserSync.reload);
 });
 
 gulp.task('lint', function() {
@@ -38,9 +41,23 @@ gulp.task('lint', function() {
         .pipe(eslint.failAfterError()); 
 });
 
-gulp.task('watch', function() {
-   gulp.watch('js/*.js', ['scripts']);
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+   	  .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('default', ['watch', 'browser-sync', 'lint', 'plumber']);
+gulp.task('watch', function() {
+   gulp.watch('js/*.js', ['scripts']);
+   gulp.watch('sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-$(function() {
+$(function () {
+	$('.navigation').on('change', function () {
+		var selectValue = $('.navigation').val();
+		// console.log(selectValue)
+		var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectValue + '.json'
+		// console.log(url)
+		url += '?' + $.param({
+			'api-key': '1f62158a878743f7a7cb556178d0130a'
+		});
+		$.ajax({
+			url: url,
+			method: 'GET',
+		}).done(function (data) {
+			console.log(data);
+			$.each(data.results, function(key, value) {
+				if(value.multimedia.length > 0) {
+				console.log(value.multimedia.length)
+			} 
+				// $('.article-list').append(<img src=' + value.artworkUrl100 + '>);
 
-	//This is my message
-	var message = 'pika piiii';
-	console.log(message); asdfasfafadfafadfd
+			})
+		}).fail(function (err) {
+			throw err;
+		});
+	})
 })
