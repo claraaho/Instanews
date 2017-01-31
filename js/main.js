769b8fb58f75bd2a43685bfc5e4cd64756146208
Clara <claraho@live.ca>
2017-01-31 11:39:18 -0800

    Cleaned up indentings

--- js/main.js
+++ js/main.js
@@ -11,35 +11,35 @@ $(function () {
 			'api-key': '1f62158a878743f7a7cb556178d0130a',
 		});
 		$.ajax({
-			url: url,
-			method: 'GET',
-		})
+				url: url,
+				method: 'GET',
+			})
 
-		.done(function (data) {
-			$('.preloader').hide()
-			$results = $('.article-list')
-			var articleInfo = ''
-			var filteredArray = data.results.filter(function (result) {
-				return result.multimedia.length >= 5;
-			}).slice(0, 12)
-			$.each(filteredArray, function (key, value) {
-				var articleContent = value.abstract
-				var articlePic = '<img src=' + value.multimedia[4].url + '>'
-				var articleLink = value.url
-				articleInfo += '<li><a href=' + articleLink + ' target="_blank">'
-				articleInfo += '<p>' + articleContent + '</p>'
-				articleInfo += articlePic
-				articleInfo += '</a></li>'
+			.done(function (data) {
+				$('.preloader').hide()
+				$results = $('.article-list')
+				var articleInfo = ''
+				var filteredArray = data.results.filter(function (result) {
+					return result.multimedia.length >= 5;
+				}).slice(0, 12)
+				$.each(filteredArray, function (key, value) {
+					var articleContent = value.abstract
+					var articlePic = '<img src=' + value.multimedia[4].url + '>'
+					var articleLink = value.url
+					articleInfo += '<li><a href=' + articleLink + ' target="_blank">'
+					articleInfo += '<p>' + articleContent + '</p>'
+					articleInfo += articlePic
+					articleInfo += '</a></li>'
+				});
+
+				// Layout styling when articles append
+				$('.menu-wrapper').toggleClass('menu-wrapper menu-change');
+				$('.nav-wrapper').toggleClass('nav-wrapper nav-change');
+				$('.copyright').toggleClass('copyright copyright-change');
+				$results.append(articleInfo).slideDown('slow');
+			}).fail(function () {
+				$('.preloader').hide()
+				$results.append('Sorry! There was a problem, please try again.');
 			});
-
-			// Layout styling when articles append
-			$('.menu-wrapper').toggleClass('menu-wrapper menu-change');
-			$('.nav-wrapper').toggleClass('nav-wrapper nav-change');
-			$('.copyright').toggleClass('copyright copyright-change');
-			$results.append(articleInfo).slideDown('slow');
-		}).fail(function () {
-			$('.preloader').hide()
-			$results.append('Sorry! There was a problem, please try again.');
-		});
 	})
 })
